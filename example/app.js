e71759a61713c36bdfa37ea4e9e773bb60b49665
wjhames <williamjasonhames@gmail.com>
2026-02-25 18:51:50 -0600

    refactor(example): consolidate root app logic into app.js

--- example/app.js
+++ example/app.js
@@ -1,4 +1,22 @@
-import { mount } from '../src/microview.js';
-import { App } from './App.js';
+import { createRouter } from '../src/router.js';
+import { h, mount } from '../src/microview.js';
+import { AppLayout } from './layouts/AppLayout.js';
+import { Home } from './pages/Home.js';
+import { About } from './pages/About.js';
+import { Todo } from './pages/Todo.js';
+import { NotFound } from './pages/NotFound.js';
+
+const routes = {
+  '/': Home,
+  '/about': About,
+  '/todos': Todo,
+  '/404': NotFound,
+};
+
+const { activeComponent } = createRouter(routes);
+
+function App() {
+  return h(AppLayout, { activeComponent });
+}
 
 mount('#root', App);
